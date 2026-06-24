88ab52d637a7da49a7ae013b0c680ce7932131a6
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-06-24 18:42:55 +0530

    loginUser

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -53,3 +53,28 @@ const registerUser = async (req, res) => {
   }
 };
 
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        upiId: user.upiId, // New Feature
+        balance: user.balance,
+        hasMpinSet: !!user.mpin, // Returns true if mpin is set
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({ message: 'Invalid email or password' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
