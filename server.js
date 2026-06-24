2f1a58e617adb89e2877448e8e1682e51e9d2e8c
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-06-24 18:17:31 +0530

    connected mongodb

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./src/config/db');
+
+
+const app = express();
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  
+});
