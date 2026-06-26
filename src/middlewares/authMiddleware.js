0fdf2168773f86cec7f7af25cebd0e51cb486896
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-06-26 17:16:26 +0530

    Check if token exists in headers

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,2 +1,11 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+
+const protect = async (req, res, next) => {
+  let token;
+
+  // Check if token exists in headers
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
