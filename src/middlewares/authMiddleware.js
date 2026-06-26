c708ce5338adfd1fe7c793c999fa69f4a5aac7c0
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-06-26 17:17:00 +0530

    Get token from header (Format is "Bearer <token>")

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -9,3 +9,6 @@ const protect = async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
+    try {
+      // Get token from header (Format is "Bearer <token>")
+      token = req.headers.authorization.split(' ')[1];
