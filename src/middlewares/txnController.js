18cc5743c56cb00b1a6b59e663c61e26295c7a50
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-06-27 22:39:24 +0530

    sendMoney

--- src/middlewares/txnController.js
+++ src/middlewares/txnController.js
@@ -5,3 +5,8 @@ const bcrypt = require('bcryptjs');
 // @desc    Send money via Phone Number OR UPI ID
 // @route   POST /api/transactions/send
 // @access  Private
+
+const sendMoney = async (req, res) => {
+  try {
+    const { receiverIdentifier, amount, mpin } = req.body; // identifier can be Phone OR UPI ID
+    const senderId = req.user._id;
