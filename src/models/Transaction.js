ce7a2489d0bd1c70478963f6c97ca511a4d686ce
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-06-29 21:13:59 +0530

    transactionSchema

---
+++ src/models/Transaction.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
