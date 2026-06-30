f3dff856a9d349700a21b95a2ea87ba1d0a04efc
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-06-30 21:59:11 +0530

    const seedUsers

--- seed.js
+++ seed.js
@@ -3,3 +3,12 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const User = require('./src/models/User');
 const Transaction = require('./src/models/Transaction');
+
+const seedUsers = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('Database Connected for Seeding...');
+
+    await User.deleteMany();
+    await Transaction.deleteMany();
+    console.log('Existing users & transactions deleted.');
