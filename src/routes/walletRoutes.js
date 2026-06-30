45752206b4a06a57a9f4daac6b54729c5407c3b0
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-06-30 21:56:41 +0530

    router.post

--- src/routes/walletRoutes.js
+++ src/routes/walletRoutes.js
@@ -4,3 +4,6 @@ const { addMoney, payBill } = require('../controllers/walletController');
 const { protect } = require('../middlewares/authMiddleware');
 
 router.post('/add-money', protect, addMoney);
+router.post('/pay-bill', protect, payBill);
+
+module.exports = router;
