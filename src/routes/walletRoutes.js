7b74c9b155a6c3618a489ca3251f266631b9cd95
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-06-30 21:55:28 +0530

    updated

---
+++ src/routes/walletRoutes.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const router = express.Router();
+const { addMoney, payBill } = require('../controllers/walletController');
+const { protect } = require('../middlewares/authMiddleware');
